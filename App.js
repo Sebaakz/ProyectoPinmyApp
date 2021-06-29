fc472aebb21a11dae73535c0aad8963d4a1818ab
Sebastián Barría <sbarria75@gmail.com>
2021-06-28 23:52:44 -0400

    Actualización de Firebase e instalación de yarn scroll
    se actualiza key de firebase e instalación de react-native-keyboard-aware-scroll-view@~0.9.1

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './app/navigations/Navigation'
 import ViewsAsMarkers from './app/screens/locales';
-import {firebaseApp} from './app/utils/firebase'
+import {firebaseApp} from './app/utils/firebase';
+import * as firebase from "firebase";
 
 
 export default function App() {
-
 
   return (
     <Navigation/>
--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDIFgzYM7K3dBRa48HjSY-vAYkKFCcUWTk",
-    authDomain: "proyectopinmymap.firebaseapp.com",
-    projectId: "proyectopinmymap",
-    storageBucket: "proyectopinmymap.appspot.com",
-    messagingSenderId: "342946970119",
-    appId: "1:342946970119:web:07a09980be3165cf07c8a2"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyCtNKlzzXGf98k-Hi2iQjqlCfs-jyBn00I",
+  authDomain: "pinmymap-a5007.firebaseapp.com",
+  projectId: "pinmymap-a5007",
+  storageBucket: "pinmymap-a5007.appspot.com",
+  messagingSenderId: "985551810585",
+  appId: "1:985551810585:web:fc1b1914225bae8291ba88"
+};
 
-  export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
